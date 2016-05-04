ee5a312b4ef568f777725d8e3fdf4b2b345148cd
dleacock <dave.leacock@gmail.com>
2016-05-04 12:16:19 -0600

    Fixed camera tracking and improved player controls.

--- Assets/Camera.cs
+++ Assets/Camera.cs
@@ -4,23 +4,31 @@ using System.Collections;
 public class Camera : MonoBehaviour {
 
     public GameObject player;
-
+    float offesetX;
+    Transform playerTransform;
 
 	// Use this for initialization
 	void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
-	
-	}
+
+        if (player != null) {
+            Debug.Log("Player not instanced");
+        }
+
+        playerTransform = player.transform;
+        offesetX = transform.position.x - playerTransform.position.x;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-        if(player != null) {
-            transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
-            //transform.Translate(new Vector3(player.transform.position.x, 0, 0));
-        } else {
-            Debug.Log("Player not instanced");
+        if(playerTransform != null) {
+            Vector3 pos = transform.position;
+            pos.x = playerTransform.position.x + offesetX;
+            transform.position = pos;
         }
+
 	
 	}
 }
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,26 +3,84 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public float speed = 0.5f;
-    public float jumpHeight = 0.2f;
-    public new Rigidbody2D rigidbody;
+    Vector3 velocity = Vector3.zero;
+    public float hoverForce = 100f;
+    public float forwardSpeed = 10f;
     
-	// Use this for initialization
-	void Start () {
-        rigidbody = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public Vector3 Gravity;
+    public Vector3 HoverVelocity;
+    public float maxSpeed = 5f;
 
-        transform.Translate(Vector3.right * Time.deltaTime);
+    bool didHover = false;
+    private Rigidbody2D playerRigidBody = null;
+
+
+    void Awake() {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
+            didHover = true;
+        }
         
-        if (Input.GetKeyDown(KeyCode.Space)) {
-      
-            rigidbody.AddForce(Vector2.up * 100f);
+    }
+    
+    void FixedUpdate() {
+        #region rigidBodyAttempt
+        //if(playerRigidBody != null) {
+        //    playerRigidBody.AddForce(Vector2.right * forwardSpeed);
 
+
+
+        //    if (didHover) {
+        //        playerRigidBody.AddForce(Vector2.up * hoverForce);
+        //        didHover = false;
+        //    }
+
+
+        //}
+
+        //float angle = 0;
+
+        //if (playerRigidBody.velocity.y < 0f) {
+        //    angle = Mathf.Lerp(0f, -90f, -playerRigidBody.velocity.y / 4f);
+        //}
+        //else if (playerRigidBody.velocity.y > 0f) {
+        //    angle = Mathf.Lerp(0f, 90f, playerRigidBody.velocity.y / 4f);
+        //}
+
+        //transform.rotation = Quaternion.Euler(0f, 0f, angle);
+        #endregion
+
+        
+        velocity.x = forwardSpeed;
+        velocity += Gravity * Time.deltaTime;
+
+        if (didHover) {
+            didHover = false;
+
+            if (velocity.y < 0f) {
+                velocity.y = 0f;
+            }
+
+            velocity += HoverVelocity;
         }
 
+        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+        transform.position += velocity * Time.deltaTime;
 
-	}
+        float angle = 0;
+
+        if (velocity.y < 0f) {
+            angle = Mathf.Lerp(0f, -90f, -velocity.y / maxSpeed);
+        }
+        else if (velocity.y > 0f) {
+            angle = Mathf.Lerp(0f, 90f, velocity.y / maxSpeed);
+        }
+
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+       
+    }
 }
